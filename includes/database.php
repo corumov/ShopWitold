3133777dfc25081708e8f665d2956c908c2f22a0
corumov <ppplusa@gmail.com>
2016-04-03 08:55:39 +0000

    testowy commit

---
+++ includes/database.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: CRM
+ * Date: 2016-04-03
+ * Time: 10:50
+ */
+mb_internal_encoding('UTF-8');
+mb_http_output('UTF-8');
+mb_http_input('UTF-8');
+mb_regex_encoding('UTF-8');
+?>
